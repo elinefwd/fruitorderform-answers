e1ddf866509ead3e7b8319870331513768617d2c
elinefwd <elinefwd@gmail.com>
2023-12-05 10:19:42 +0100

    with-components

---
+++ src/assets/components/Button.jsx
@@ -0,0 +1,13 @@
+function Button({ clickHandler, children, type, disabled }) {
+    return (
+        <button
+            onClick={clickHandler}
+            type={type}
+            disabled={disabled || false}
+        >
+            {children}
+        </button>
+    );
+}
+
+export default Button;
